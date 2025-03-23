8cde189fe1b0135762009f6788e1441eee387deb
ashraf <nurthern2016@gmail.com>
2025-03-23 17:32:56 +0600

    useNavigate added to protected route

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Navigate } from "react-router";
+import { useNavigate } from "react-router";
 
 
 
 const ProtectedRoute = ({children}: {children:React.ReactNode}) => {
+	const navigate= useNavigate()
 	const isAuthenticated = Boolean(localStorage.getItem('token'));
-	return isAuthenticated ? <>{children}</> : <Navigate to='/login' />
+	return isAuthenticated ? <>{children}</> : navigate('/login', {replace: true});
 }
 export default ProtectedRoute;
